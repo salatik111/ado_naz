dda103f27a8a65ed119773ddbac606273e5fbe14
salatik111 <115220008+salatik111@users.noreply.github.com>
2022-11-05 11:42:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let bit = 20;
+if (bit <= 5) {
+    console.log ( 'Да' );
+} else {
+    console.log ('Нет');
+}
+
+let mia = 20;
+let izo = "11";
+let nazi = 48;
+alert(mia + izo++ - nazi );
+
+
+const brain = 101;
+if (brain > 100) {
+    console.log('Decline');
+}  else {
+    console.log('Decline');
+}
